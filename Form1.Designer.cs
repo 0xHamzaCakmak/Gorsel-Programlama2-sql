0442f66c7cdabe7199a778a2ce6968419259d3f6
Hamza Çakmak <reelaktor@gmail.com>
2019-03-10 18:38:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace sql_komutları_gp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(536, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(271, 303);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(536, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "insert into1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(536, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "insert into TB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(304, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(536, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "insert into parmt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(536, 162);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(536, 191);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "delete LB secili";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(536, 220);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(121, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Update";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(536, 271);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(121, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "TXT den DB ye aktar";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(536, 301);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(121, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "DB den TXT aktar";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 423);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace sql_komutları_gp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //local veritabanındaki bilgileri select ile cagırıp listbox1 e ekleyen program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from ogrenci order by ders_id desc", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            listBox1.Items.Clear();
+            while (dr.Read())
+            {
+                listBox1.Items.Add(dr["ders_id"].ToString() + "\t" + dr["ders_name"].ToString());
+            }
+
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //local veritabanındaki stunlara kayıt ekleyen program(İNSERT İNTO)
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ogrenci(ders_name) values ('ingilizce')", con);
+            int adet = cmd.ExecuteNonQuery();
+            MessageBox.Show(adet.ToString() + "kayıt eklendi");
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //textbox1 de yazan metni local veritabanındaki stuna kayıt ekleyen program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ogrenci(ders_name) values ('"+textBox1.Text+"')", con);
+            int adet = cmd.ExecuteNonQuery();
+            MessageBox.Show(adet.ToString() + "kayıt eklendi");
+            con.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //textbox1de yazan metni parametre olarak local veritabanında stuna kayıt  ekleyen program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ogrenci(ders_name) values(@ders_name)", con);
+            cmd.Parameters.AddWithValue("@ders_name", textBox1.Text);
+            int adet = cmd.ExecuteNonQuery();
+            MessageBox.Show(adet.ToString() + "kayıt eklendi");
+            con.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //local veritabanından kayıt silen program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from ogrenci where ders_id=8", con);
+            int adet = cmd.ExecuteNonQuery();
+            MessageBox.Show(adet.ToString() + "kayıt silindi");
+            con.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //listbox1de secili olan bilgiyi local veri tabanından silen program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from ogrenci where ders_id=@id", con);
+            int numara = Convert.ToInt16(listBox1.SelectedItem.ToString().Split('\t')[0]);
+            cmd.Parameters.AddWithValue("@id", numara);
+            int adet = cmd.ExecuteNonQuery();
+            MessageBox.Show(adet.ToString() + " kayıt silindi");
+            con.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //listbox1 de secılı olan metni veri tabanında buyuk harve ceviren program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update ogrenci  set ders_name=Upper(ders_name) where ders_id=@id", con);
+            int numara = Convert.ToInt16(listBox1.SelectedItem.ToString().Split('\t')[0]);
+            cmd.Parameters.AddWithValue("@id", numara);
+            int adet = cmd.ExecuteNonQuery();
+            MessageBox.Show(adet.ToString() + " kayıt guncellendi");
+            con.Close();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //deneme txt adlı dosyanın ıcındekı metni kayıt bılgısı olarak locak veritabanına ekleyen program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ogrenci(ders_name) values(@ders_ismi)", con);
+            cmd.Parameters.Add("@ders_ismi", SqlDbType.NVarChar);
+            StreamReader sr = new StreamReader("kategoriler.txt");
+            while (!sr.EndOfStream)
+            {
+                string satir = sr.ReadLine();
+                cmd.Parameters["@ders_ismi"].Value = satir;
+                cmd.ExecuteNonQuery();
+            }
+            MessageBox.Show("aktarım tamam");
+            sr.Close();
+            con.Close();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // local DB de bulunan verileri txt dosyasına aktaran program
+            string bag = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\reela\onedrive\documents\visual studio 2013\Projects\sql komutları gp1\sql komutları gp1\deneme1.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(bag);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from ogrenci", con);
+            StreamWriter sw = new StreamWriter("databse.txt");
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                string satir = (dr["ders_id"].ToString() + "\t" + dr["ders_name"].ToString());
+                sw.WriteLine(satir);
+            }
+            MessageBox.Show("aktarım tamamlandı");
+            sw.Close();
+            con.Close(); ;
+        }
+    }
+}
